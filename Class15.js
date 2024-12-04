989eb8d3a499620a79233feaa0c137d3708d8181
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 11:44:57 +0600

    Create Class15.js

---
+++ Class15.js
@@ -0,0 +1,12 @@
+Defining and Calling Functions
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Alice"));
+
+Arrow Functions
+
+
+const sum = (a, b) => a + b;
+console.log(sum(2, 3));
