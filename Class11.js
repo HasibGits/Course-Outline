ab2bec69fe8c78e0d69977f1e0fb820838463f41
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 11:39:12 +0600

    Create Class11.js

---
+++ Class11.js
@@ -0,0 +1,13 @@
+What is JavaScript?
+JavaScript is a versatile, high-level programming language for adding interactivity to web pages.
+Embedding JavaScript in HTML
+
+
+
+<!-- Inline Script -->
+<script>
+    alert("Hello, JavaScript!");
+</script>
+
+<!-- External Script -->
+<script src="script.js"></script>
