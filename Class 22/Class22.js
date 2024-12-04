74353e8bade128d4017466a30f8c4f4be327e54f
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 16:46:07 +0600

    Create Class22.js

---
+++ Class 22/Class22.js
@@ -0,0 +1,30 @@
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+
+document.getElementById("demo").innerHTML =
+"Hello Dolly!";
+
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph.";
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+const pi = 3.14;
+let person = "John Doe";
+let answer = 'Yes I am!';
