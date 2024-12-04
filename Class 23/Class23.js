7f58418158446ebc0bad9e6cced850979f0d51b8
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 16:48:21 +0600

    Create Class23.js

---
+++ Class 23/Class23.js
@@ -0,0 +1,33 @@
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+let x = 5;
+let z = Math.pow(x,2);
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+let x = 5;
+let y = 5;
+let z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
