c767bae2781e9b789245c8b5fb96cdd03ed0d208
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 11:45:38 +0600

    Create Class16.js

---
+++ Class16.js
@@ -0,0 +1,9 @@
+Selecting Elements
+
+let element = document.getElementById("myDiv");
+let query = document.querySelector(".className");
+
+Modifying Elements
+
+element.textContent = "New Content";
+element.style.color = "blue";
