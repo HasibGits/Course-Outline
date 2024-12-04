89cc69367525b8ab5cdf429740ca40649422c3a3
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 11:48:19 +0600

    Create Class19.js

---
+++ Class19.js
@@ -0,0 +1,13 @@
+Template Literals
+
+let name = "John";
+console.log(`Hello, ${name}!`);
+
+Destructuring
+
+let { name, age } = { name: "Alice", age: 25 };
+
+Spread Operator
+
+let arr = [1, 2, 3];
+let newArr = [...arr, 4];
