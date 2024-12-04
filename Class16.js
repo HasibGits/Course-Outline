67ab02cc742966d192b365bd788f51061faf1162
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 14:00:23 +0600

    Update and rename Class16.js to Class 16/Class16.css

--- Class16.js
+++
@@ -1,9 +0,0 @@
-Selecting Elements
-
-let element = document.getElementById("myDiv");
-let query = document.querySelector(".className");
-
-Modifying Elements
-
-element.textContent = "New Content";
-element.style.color = "blue";
