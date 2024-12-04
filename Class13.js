a5779a8ddfd83b41ec4b5aa72222ab6587edbf00
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 11:41:26 +0600

    Create Class13.js

---
+++ Class13.js
@@ -0,0 +1,12 @@
+Common Operators
+
+
+// Arithmetic
+let sum = 10 + 5;
+
+// Comparison
+let isEqual = 5 == "5"; // true
+let isStrictEqual = 5 === "5"; // false
+
+// Logical
+let isTrue = true && false; // false
