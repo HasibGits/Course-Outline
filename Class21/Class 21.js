005b8959eb33412228a3c3b4c1c6bcbc74e4e726
MD HASIBUZZAMAN BHUIYAN <106241572+HasibGits@users.noreply.github.com>
2024-12-04 16:44:02 +0600

    Update and rename Class 21 to Class21/Class 21.js

---
+++ Class21/Class 21.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+<body>
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+<h1>My First Web Page</h1>
+<p>My first paragraph.</p>
+
+<button type="button" onclick="document.write(5 + 6)">Try it</button>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+window.alert(5 + 6);
+</script>
+
+</body>
+</html>
